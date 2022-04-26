6a6be8add381e026edba9c760f7603ad3a6ea762
LuisMiguelBr <luismiguelbravolopez3@gmail.com>
2022-04-26 10:08:17 +0200

    Primer commit

---
+++ src/main/java/EjercicioContarLetras.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class EjercicioContarLetras {
+
+    public static void main(String []args) {
+        char letras[] = new char[];
+        String input = "";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Dime un una palabra");
+        input=sc.nextLine();
+        letras=input.toCharArray();
+
+
+
+
+    }
+
+
+    public static void contar(char [] letas){
+
+
+
+    }
+}
