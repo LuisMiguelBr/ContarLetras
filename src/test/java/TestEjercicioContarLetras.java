e4a9fc3e6704ac2aa34730fe482e594dc6dc52c4
LuisMiguelBr <luismiguelbravolopez3@gmail.com>
2022-04-26 13:30:02 +0200

    Septimo commit

--- src/test/java/TestEjercicioContarLetras.java
+++ src/test/java/TestEjercicioContarLetras.java
@@ -7,15 +7,19 @@ public class TestEjercicioContarLetras {
     public void Test1(){
 
         String prueba="Holaaa!";
-        String resultadoEsperado="{a=3, l=1, h=1, o=3, !=1}";
+        String resultadoEsperado="{a=3, !=1, h=1, l=1, o=1}";
         String resultado= EjercicioContarLetras.contarRepeticiones(prueba);
         Assertions.assertEquals(resultadoEsperado,resultado);
         System.out.println("Test1 realizado");
-
-
+    }
+    public void Test2(){
+        String prueba=null;
+        String resultadoEsperado=null;
+        String resultado= EjercicioContarLetras.contarRepeticiones(prueba);
+        Assertions.assertEquals(resultadoEsperado,resultado);
+        System.out.println("Test2 realizado");
 
     }
-
 
 
 }
